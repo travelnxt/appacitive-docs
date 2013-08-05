ba8510f02298ddbbc12b18a42f5117e9cedbf9e3
Yash Sinha <ysinha@appacitive.com>
2013-08-05 12:58:33 +0530

    Removed unused variables and improved console output for ps script

--- gzipanduploadtos3.ps1
+++ gzipanduploadtos3.ps1
@@ -4,6 +4,7 @@ param([string]$s3cmdpath="D:\utils\s3cmd\s3cmd",[string]$s3bucket="helptest.appa
 if(Test-Path .git)
 {
 	#Remove git folder
+	Write-Host "Removing git folder"
 	Remove-Item -recurse -force .git
 }
 
@@ -15,18 +16,16 @@ $compressextensions = (".html",".js",".xml",".css")
 
 foreach($file in $files)
 {
-  "Working with file " + $file.FullName
   if($compressextensions.Contains($file.Extension))
   {
-    #Uses gzip provided by cygwin
     Write-Gzip -Level 9 $file.FullName
 	Remove-Item $file.FullName
 	Rename-Item ($file.FullName+".gz") $file.Name
-	"Compressing "+$file.FullName
+	Write-Host "Compressing" $file.FullName
   }
   else
   {
-    $file.FullName + "does not need to be compressed"
+    Write-Host $file.FullName + "does not need to be compressed"
 	$otherfiles += $file
   }
 }
@@ -37,11 +36,8 @@ $onemonth = 2592000
 $oneweek = 604800
 $oneday = 86400
 $onehour = 3600
-$oneyearstring = ((Get-Date).AddSeconds(31557600)).ToString('r')
-$onemonthstring = ((Get-Date).AddSeconds(2592000)).ToString('r')
-$oneweekstring = ((Get-Date).AddSeconds(604800)).ToString('r')
-$onedaystring = ((Get-Date).AddSeconds(86400)).ToString('r')
-$onehourstring = ((Get-Date).AddSeconds(3600)).ToString('r')
+$onedayrfc1123 = ((Get-Date).AddSeconds(86400)).ToString('r')
+
 
 # Delete current deployment
 "Emptying current bucket contents"
@@ -52,7 +48,7 @@ $workingdir = [regex]::Escape((Get-Location).ToString()) #Escapes any special ch
 foreach($file in $files)
 {
   $s3path = "s3://" + $s3bucket + (($file.FullName).ToString() -replace $workingdir,"" -replace "\\","/")
-  "Uploading " + $file.Name + " to " + $s3path
+  Write-Host "Uploading" $file.Name "to" $s3path
   switch($file.Extension)
   {
     ".html" 
@@ -88,6 +84,7 @@ foreach($file in $files)
 }
 
 # Replace index.html with short expire time
-python $s3cmdpath put --no-progress --acl-public --no-preserve --add-header="Cache-Control:public, max-age=$oneday, must-revalidate" --add-header="Expires:$onedaystring"  --add-header="Content-Encoding:gzip" --mime-type="text/html; charset=utf-8" $indexdoc s3://$s3bucket/$indexdoc
+Write-Host "Setting short expiry time for" $indexdoc
+python $s3cmdpath put --no-progress --acl-public --no-preserve --add-header="Cache-Control:public, max-age=$oneday, must-revalidate" --add-header="Expires:$onedayrfc1123"  --add-header="Content-Encoding:gzip" --mime-type="text/html; charset=utf-8" $indexdoc s3://$s3bucket/$indexdoc
 
 
