13215fef750cd6c406ee5ad29a28c85cb0f3b987
Nikhil Prasad <nikhil.prasad@gmail.com>
2013-12-12 01:47:19 +0530

    added config for v1.0 and v0.9 files.

--- scripts/buildScripts/config.js
+++ scripts/buildScripts/config.js
@@ -1,13 +1,20 @@
 module.exports = {
-	files: [{
+	files: [
+	/*
+	{
 		htmlFile: './index.html',
 		type: 'file',
 		markdownFile: './index.md'
+	},*/
+	{
+		htmlFile: './v0.9/index.html',
+		type: 'file',
+		markdownFile: './v0.9/index.md'
+	},
+	{
+		htmlFile: './v1.0/index.html',
+		type: 'file',
+		markdownFile: './v1.0/index.md'
 	}
-	/*, {
-		htmlFile: './jssdk.html',
-		type: 'github',
-		repo: 'chiragsanghvi/JavascriptSDK'
-	}*/
 	]
 };
